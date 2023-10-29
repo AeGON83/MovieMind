c9db1f0e5625da91b5b21c5fbb9cb47f932428e2
bharat <bharatladva77@gmail.com>
2023-10-30 02:35:44 +0530

    fix bugs8.2

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,58 @@
-/** @format */
+// /** @format */
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import dotenv from "dotenv";
+// import path from "path";
+// dotenv.config();
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	root: "./", // project root
+// 	base: "./", // base path for the project
+// 	build: {
+// 		outDir: "dist", // the output directory for the build
+// 		rollupOptions: {
+// 			input: "./index.html", // replace this with your entry point if it's not 'src/index.html'
+// 		},
+// 	},
+// 	define: {
+// 		"process.env": {
+// 			REACT_APP_TMDB_API_KEY: process.env.REACT_APP_TMDB_API_KEY,
+// 		},
+// 	},
+// });
+
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
-import path from "path";
+import path from "path"; // Import the path module
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	root: "./", // project root
-	base: "./", // base path for the project
+	base: "./",
 	build: {
-		outDir: "dist", // the output directory for the build
+		outDir: "dist",
+		assetsDir: "",
 		rollupOptions: {
-			input: "./index.html", // replace this with your entry point if it's not 'src/index.html'
+			input: {
+				main: path.resolve(__dirname, "index.html"), // Use path.resolve to get the correct file path
+			},
+		},
+	},
+	server: {
+		fs: {
+			allow: [".", ".."], // Allow serving files outside of the default directories
 		},
 	},
 	define: {
 		"process.env": {
-			REACT_APP_TMDB_API_KEY: process.env.REACT_APP_TMDB_API_KEY,
+			REACT_APP_TMDB_API_KEY:
+				process.env.REACT_APP_TMDB_API_KEY
+			
 		},
 	},
 });
